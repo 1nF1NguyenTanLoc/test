cd59c11d22821038c8ecb3236a04ca7e6e741522
NguyenTanLoc <locpro22vip@gmail.com>
2023-11-14 22:14:51 +0700

    SCPFix

--- text.cpp
+++ text.cpp
@@ -15,6 +15,11 @@ bool isPerfectNumber(int number) {
     return (sum == number);
 }
 
+bool isPerfectSquare(int number) {
+    int sqrtNumber = sqrt(number);
+    return (sqrtNumber * sqrtNumber == number);
+}
+
 int main() {
     // Sử dụng hàm để kiểm tra số hoàn thiện
     int testNumber = 28; // Thay đổi số cần kiểm tra ở đây
@@ -23,6 +28,15 @@ int main() {
         std::cout << testNumber << " là số hoàn thiện." << std::endl;
     } else {
         std::cout << testNumber << " không phải là số hoàn thiện." << std::endl;
+    }
+    
+    // Sử dụng hàm để kiểm tra số chính phương
+    int testNumber = 16; // Thay đổi số cần kiểm tra ở đây
+
+    if (isPerfectSquare(testNumber)) {
+        std::cout << testNumber << " là số chính phương." << std::endl;
+    } else {
+        std::cout << testNumber << " không phải là số chính phương." << std::endl;
     }
 
     return 0;
